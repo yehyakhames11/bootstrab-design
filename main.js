30f72cce5adf69412e535f0f28d3e486f0fab017
yehyakhames11 <122730824+yehyakhames11@users.noreply.github.com>
2023-08-18 17:54:42 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+let btn = document.getElementById('btn-top');
+
+window.onscroll = function() {
+    if(scrollY >= 400 ) {
+        btn.style.display = 'block';
+    }else {
+        btn.style.display = 'none';
+    }
+
+    btn.onclick = function() {
+        scroll({
+            left: 0,
+            top: 0,
+            behavior: "smooth",
+        })
+    }
+}
